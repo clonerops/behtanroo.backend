9ef7cfac325dea00ada5282d17827b0b0053ee2a
Cloner ops <clonerops@gmail.com>
2024-02-26 15:46:24 +0330

    document

--- src/controlles/documentController.js
+++ src/controlles/documentController.js
@@ -4,7 +4,7 @@ const documentController = {
     addDocument: async (req, res, next) => {
         const { title } = req.body;
 
-        const document = await Document.create({title})
+        const document = await Document.create({ title })
         return res.status(201).json({
             success: true,
             data: document
@@ -17,7 +17,43 @@ const documentController = {
             success: true,
             data: documents
         })
-    }
+    },
+
+    getDocument: async (req, res, next) => {
+        const document = await Document.findOne({ where: { id: req.params.id } })
+        return res.status(200).json({
+            success: true,
+            data: document
+        })
+    },
+
+    updateDocument: async (req, res, next) => {
+        const findDocument = await Document.findOne({ where: { id: req.params.id } })
+        if (!findDocument) {
+            return res.status(400).json({
+                success: false,
+                message: "پرونده یافت نشد"
+            })
+        } else {
+            await findDocument.update({ title: req.body.title })
+            return res.status(200).json({ success: true })
+        }
+    },
+
+    deleteDocument: async (req, res, next) => {
+        const findDocument = await Document.findOne({ where: { id: req.params.id } })
+        if (!findDocument) {
+            return res.status(400).json({
+                success: false,
+                message: "پرونده یافت نشد"
+            })
+        } else {
+            await findDocument.destroy()
+            return res.status(200).json({ success: true })
+        }
+    },
+
+
 }
 
 module.exports = documentController
