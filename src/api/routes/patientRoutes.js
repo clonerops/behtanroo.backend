19628ad36a684e84ad46e04d8f9395bec6e8fd8f
clonerops <114849678+clonerops@users.noreply.github.com>
2024-02-26 09:33:44 -0800

    excel

--- src/api/routes/patientRoutes.js
+++ src/api/routes/patientRoutes.js
@@ -4,6 +4,7 @@ const patientController = require('../../controlles/patientController.js')
 const router = express.Router()
 
 router.post('/create', patientController.createPatient)
+router.get('/exportExcel', patientController.exportExcelPatientList)
 router.get('/lists', patientController.getAllPatients)
 router.get('/:id', patientController.getPatientsById)
 router.get('/:id/referral', patientController.getPatientReferralById)
