ceb290cbe32fce759768bc4ae1ae3827a29d7c41
clonerops <114849678+clonerops@users.noreply.github.com>
2024-04-07 10:27:36 -0700

    ref

--- src/api/routes/patientRoutes.js
+++ src/api/routes/patientRoutes.js
@@ -5,6 +5,7 @@ const authMiddleware = require('../middlewares/auth.js')
 const router = express.Router()
 
 router.post('/create', authMiddleware.authenticateToken, patientController.createPatient)
+router.put('/:id/edit', authMiddleware.authenticateToken, patientController.editPatient)
 router.get('/exportExcel', authMiddleware.authenticateToken, patientController.exportExcelPatientList)
 router.get('/lists', authMiddleware.authenticateToken, patientController.getAllPatients)
 router.get('/:id', authMiddleware.authenticateToken, patientController.getPatientsById)
--- src/controlles/patient-document-Controller.js
+++ src/controlles/patient-document-Controller.js
@@ -5,13 +5,13 @@ const PatientDocument = require("../models/patient_document")
 
 const patientDocumentController = {
     createPatientDocument: async (req, res, next) => {
-        const {documentId, patientId, description} = req.body 
+        const { documentId, patientId, description } = req.body
 
         const result = await PatientDocument.create({
-            documentId, 
-            patientId, 
+            documentId,
+            patientId,
             description,
-            documentCode: Math.floor(Math.random()*1000000)
+            documentCode: Math.floor(Math.random() * 1000000)
         })
 
         return res.status(201).json({
@@ -50,11 +50,45 @@ const patientDocumentController = {
                 {
                     documentId: req.params.documentId
                 }],
-        }, include: [Patient]})
+            }, include: [Patient]
+        })
         return res.status(200).json({
             success: true,
             data: patientDocument
         })
+    },
+
+    deletePatientDocument: async (req, res, next) => {
+        try {
+            const findPatiendDocument = await PatientDocument.findOne({
+                where: {
+                    [Op.and]: [
+                        {
+                            patientId: req.params.patientId,
+                        },
+                        {
+                            documentId: req.params.documentId
+                        }
+                    ]
+                }
+            })
+
+            if(!findPatiendDocument) {
+                return res.status(400).json({
+                    success: true,
+                    message: "پرونده بیمار یافت نشد"
+                })
+            } else {
+                findPatiendDocument.destroy()
+                return res.status(200).json({
+                    success: true,
+                    message: "پرونده بیمار با موفقیت حذف گردید"
+                })
+            }
+
+        } catch (error) {
+            return res.status(500).json({ message: "Server Error" })
+        }
     }
 }
 
